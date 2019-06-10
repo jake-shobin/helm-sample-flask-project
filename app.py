9b2cd23e40ea71bc8c13ee898d543c2897648da6
Nguyen Xuan Dung <dung.nx@teko.vn>
2019-06-10 15:23:47 +0700

    adding celery instance

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from celery import Celery
+
+
 app = Flask(__name__)
+
+celery = Celery(__name__, broker="redis://redis-master:6379", backend="redis://redis-master:6379")
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@celery.task
+def add(x, y):
+    return x + y
